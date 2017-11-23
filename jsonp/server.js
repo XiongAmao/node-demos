5974d52f0117647c07fb9380750e0afa836dd70b
XiongAmao <onycat@live.cn>
2017-11-23 21:00:41 +0800

    Add HTML server

--- jsonp/server.js
+++ jsonp/server.js
@@ -3,19 +3,33 @@ var data = {
     age: 18,
     job: 'Front-End Engineer'
 }
-
+var port = 8888
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 
 http.createServer(function (req, res) {
     var params = url.parse(req.url, true)
-    if(params.query && params.query.callback){
+    console.log(`Request for ${params.pathname} received.`)
+    if(!params.query.callback){
+        console.log('////////')
+        fs.readFile('./index.html', function(err, data){
+            if(err){
+                console.log(err)
+                res.writeHead(404, {'Content-type': 'text/html'})
+            }else{
+                res.writeHead(200, {'Content-type':'text/html'})
+                res.write(data.toString())
+            }
+            res.end()
+        })
+    }else if(params.query && params.query.callback){
+        console.log('jsonp')
         var str = params.query.callback + '(' + JSON.stringify(data) +')'
         res.end(str)
     }else{
         res.end(JSON.stringify(data))
     }
-    console.log(params) 
-}).listen(8888, function(){
-    console.log('Server is listening on port 8888')
+}).listen(port, function(){
+    console.log(`Server is listening on port ${port}. click: http://localhost:${port}`)
 })
