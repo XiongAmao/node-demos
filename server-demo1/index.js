e31172704b8dff0d9c8f0c339998c883ea0bbdd7
XiongAmao <onycat@live.cn>
2017-12-04 21:34:23 +0800

    Update

--- server-demo1/index.js
+++ server-demo1/index.js
@@ -29,26 +29,40 @@ http.createServer(function (req, res) {
   // 第二个参数true时，将会使 query属性转成对象
   var reqPath = url.parse(req.url, true)
 
-  fs.readFile(path.join('/', reqPath.pathname), function (err, file) {
-    if (err) {
-      console.log('Can not find the file.')
-      res.writeHead(404)
-      res.end('找不到该路径的文件')
-    }
-    res.writeHead(200)
-    res.end(file)
-  })
+  if (reqPath.pathname) {
+    fs.readFile(path.join('/', reqPath.pathname), function (err, file) {
+      if (err) {
+        console.log('Can not find the file.')
+        res.writeHead(404)
+        res.end('找不到该路径的文件')
+      }
+      res.writeHead(200)
+      res.end(file)
+    })
+  }
 
   // 查询字符串
   var query = reqPath.query
   console.log('do something with query string')
-  console.log(query)
+  if(Object.keys(query).length) console.log(query)
+
 
   // Cookie
+  cookieHandle(req, res)
+
+  // res.writeHead(200, {
+  //   'Content-Type': 'text/plain'
+  // })
+  // res.end('Hello World\n')
+}).listen(port, '127.0.0.1')
+console.log('server running at http://127.0.0.1:' + port)
+
+function cookieHandle(req, res) {
   req.cookies = parseCookie(req.headers.cookie)
 
   if (!req.cookies.isVisit) {
-    console.log('cookies now')
+    console.log('Set cookies')
+    res.setHeader('Set-Cookie', cookiesSerializer('isVisit', '1'))
     res.writeHead(200, {
       'Content-type': 'text/plain; charset=UTF-8'
     })
@@ -60,13 +74,9 @@ http.createServer(function (req, res) {
     res.end('欢迎再次光临本店')
   }
 
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.end('Hello World\n')
-}).listen(port, '127.0.0.1')
-console.log('server running at http://127.0.0.1:' + port)
+}
 
+// 这里是处理 cookie 中间件
 function parseCookie(cookie) {
   var cookies = {}
   if (!cookie) {
@@ -78,4 +88,20 @@ function parseCookie(cookie) {
     cookies[pair[0].trim()] = pair[1]
   }
   return cookies
+}
+
+// 为了设置cookie的可选参数，我们使用一个函数序列化cookie的值，用于配置它
+function cookiesSerializer(name, val, option) {
+  var pairs = [`${ encodeURIComponent(name) }=${ encodeURIComponent(val) }`]
+  option = option || {}
+  // 需要处理以下的参数：
+  // path || Domain || Max-Age || Expires || HttpOnly || Secure
+  option.path && pairs.push(`Path=${option.path}`)
+  option.domain && pairs.push(`Domain=${option.domain}`)
+  option.maxAge && pairs.push(`Max-Age=${option.maxAge}`)
+  option.expires && pairs.push(`Expires=${option.expires}`)
+  option.httpOnly && pairs.push(`HttpOnly`)
+  option.secure && pairs.push(`Secure`)
+
+  return pairs.join(';')
 }
